e46976414f68c4d8bac5836d83f2deec396145ca
AYUSH VISHWAKARMA <ayush933571@gmail.com>
2026-02-19 10:30:14 +0530

    Matrix problem

---
+++ Techincal Skills/RotateMatrix.java
@@ -0,0 +1,23 @@
+// rotate a matrix by 90 degree
+public class RotateMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3},
+                        {4,5,6},
+                        {7,8,9}}; 
+        int n = arr.length;
+        System.out.println("Original matrix:");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Rotated matrix by 90 degrees:");
+        for(int i=0;i<n;i++){
+            for(int j=n-1;j>=0;j--){
+                System.out.print(arr[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- Techincal Skills/rowAndColumnSum.java
+++ Techincal Skills/rowAndColumnSum.java
@@ -3,12 +3,16 @@ public class rowAndColumnSum {
         int arr[][] = {{1,2,3},
                         {4,5,6},
                         {7,8,9}};
-        int colSum =0;
-        int rowSum=0;
+        System.out.println(" ------------------------------sum of Rows and Columns:");
         for(int i=0;i<arr.length;i++){
+            int rowSum =0;
+            int colSum =0;
             for(int j=0;j<arr.length;j++){
-
+                rowSum += arr[i][j];
+                colSum += arr[j][i];
             }
+            System.out.println("Sum of row " + i + " is: " + rowSum);
+            System.out.println("Sum of column " + i + " is: " + colSum);
         }
     }
 }
